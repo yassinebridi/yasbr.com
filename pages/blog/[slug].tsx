4231f340e65d18a27d0f8ae15c12fec788ebccc5
Yassine Bridi <ybridi@gmail.com>
2021-05-20 23:37:08 +0100

    Use custom Image components, but comment it out for now

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -61,6 +61,38 @@ const BlogPost: React.FC<BlogPostProps> = ({ post, blocks }) => {
                     : `w_${width},h_${height}`;
                 return `https://res.cloudinary.com/yasbr/image/fetch/f_auto,c_limit,${resolution}/${img}`;
               }}
+              // customBlockComponents={{
+              //   image: (props) => {
+              //     const value = props.blockValue as ContentValueType;
+              //     const format = value.format;
+              //     const width = format.block_width;
+              //     const height = format.block_height;
+              //     const resolution =
+              //       width === undefined || height === undefined
+              //         ? 'w_400'
+              //         : `w_${width},h_${height}`;
+              //     const src = value.format.display_source;
+              //     return (
+              //       <div
+              //         style={{
+              //           position: 'relative',
+              //           width: `${width}px`,
+              //           height: `${height}px`,
+              //         }}
+              //       >
+              //         <Image
+              //           src={src}
+              //           alt="Yassine Bridi's avatar"
+              //           loader={(loader) =>
+              //             `https://res.cloudinary.com/yasbr/image/fetch/f_auto,c_limit,${resolution}/${loader.src}`
+              //           }
+              //           layout="fill"
+              //           objectFit="cover"
+              //         />
+              //       </div>
+              //     );
+              //   },
+              // }}
             />
           </section>
         </article>
