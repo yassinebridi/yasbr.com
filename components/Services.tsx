2c21299ab022aa39fda2671caad9136aa8a352f4
Yassine Bridi <ybridi@gmail.com>
2021-05-30 17:43:53 +0100

    Services

--- components/Services.tsx
+++ components/Services.tsx
@@ -9,10 +9,12 @@ export interface ServicesProps {
 const Services: React.FC<ServicesProps> = ({ items }) => {
   return (
     <div className="py-16">
-      <div className="flex items-center max-w-4xl mx-auto">
+      <div className="flex items-center max-w-4xl px-4 mx-auto">
         <div className="w-full">
           <div className="font-normal text-center">
-            <h3 className="uppercase text-md text-primary-500">Services</h3>
+            <h3 className="uppercase text-md text-primary-500 text-primary-400">
+              Services
+            </h3>
             <h2 id="services" className="text-3xl font-extrabold">
               <a href="#services" title="services" className="ringify">
                 What I offer
@@ -21,7 +23,7 @@ const Services: React.FC<ServicesProps> = ({ items }) => {
           </div>
 
           <div className="mt-12">
-            <ul className="grid grid-cols-3 gap-8">
+            <ul className="grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-3 sm:gap-8">
               {items.map((item, i) => {
                 return <ServiceCard key={i} item={item} />;
               })}
@@ -42,11 +44,11 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ item }) => {
   return (
     <li
       className={clsx(
-        'relative flex flex-col p-6 border-2 border-dashed border-primary-300'
+        'relative flex flex-col p-6 border-2 border-dashed dark:border-primary-600 border-primary-300'
       )}
     >
       <span className="absolute -top-6">
-        <div className="w-12 h-12 p-3 text-white rounded-full bg-primary-100 group-hover:bg-white group-hover:text-primary-400 group-hover:shadow-md">
+        <div className="w-12 h-12 p-3 text-white rounded-full dark:bg-primary-500 bg-primary-100 group-hover:bg-white group-hover:text-primary-400 group-hover:shadow-md">
           <Image
             alt={item.Title}
             src={item.Icon[0].rawUrl}
@@ -56,10 +58,10 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ item }) => {
           />
         </div>
       </span>
-      <span className="mt-3 font-extrabold text-gray-900 text-md group-hover:text-white">
+      <span className="mt-3 text-sm font-extrabold dark:text-primary-100 text-primary-900 sm:text-md group-hover:text-white">
         {item.Title}
       </span>
-      <span className="mt-2 text-sm text-gray-800 group-hover:text-white">
+      <span className="mt-2 text-xs sm:text-sm dark:text-primary-200 text-primary-800 group-hover:text-white">
         {item.Desc}
       </span>
     </li>
