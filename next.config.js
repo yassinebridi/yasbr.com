5b35c776cadca99e63b6146948e11353a84ffa19
Yassine Bridi <ybridi@gmail.com>
2021-09-16 14:11:39 +0100

    Re-enable security headers

--- next.config.js
+++ next.config.js
@@ -44,12 +44,12 @@ module.exports = {
       destination: '/api/rss',
     },
   ],
-  // headers: async () => [
-  //   {
-  //     source: '/(.*)',
-  //     headers: securityHeaders,
-  //   },
-  // ],
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: securityHeaders,
+    },
+  ],
   webpack: (config, { dev, isServer }) => {
     // if (isServer) {
     //   require('./scripts/generate-sitemap');
@@ -70,51 +70,51 @@ module.exports = {
 };
 
 // https://securityheaders.com
-// const ContentSecurityPolicy = `
-//   default-src 'self';
-//   script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com vitals.vercel-insights.com plausible.io;
-//   child-src *.youtube.com *.google.com *.twitter.com;
-//   style-src 'self' 'unsafe-inline' *.googleapis.com;
-//   img-src * blob: data:;
-//   media-src 'none';
-//   connect-src *;
-// `;
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com vitals.vercel-insights.com plausible.io;
+  child-src *.youtube.com *.google.com *.twitter.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+`;
 
-// const securityHeaders = [
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-//   {
-//     key: 'Content-Security-Policy',
-//     value: ContentSecurityPolicy.replace(/\n/g, ''),
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-//   {
-//     key: 'Referrer-Policy',
-//     value: 'origin-when-cross-origin',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-//   {
-//     key: 'X-Frame-Options',
-//     value: 'DENY',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-//   {
-//     key: 'X-Content-Type-Options',
-//     value: 'nosniff',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-//   {
-//     key: 'X-DNS-Prefetch-Control',
-//     value: 'on',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-//   {
-//     key: 'Strict-Transport-Security',
-//     value: 'max-age=31536000; includeSubDomains; preload',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-//   // Opt-out of Google FLoC: https://amifloced.org/
-//   {
-//     key: 'Permissions-Policy',
-//     value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
-//   },
-// ];
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  // Opt-out of Google FLoC: https://amifloced.org/
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+];
