97efe8810f87fe9ac2f0cc1d7ffeb1aef2c91f9f
Yassine Bridi <ybridi@gmail.com>
2021-12-01 22:44:34 +0100

    Add google analytics

---
+++ lib/gtag.ts
@@ -0,0 +1,18 @@
+export const pageview = (url: string) => {
+  if (typeof window?.gtag !== 'undefined') {
+    console.log('window: ', window.gtag);
+    window?.gtag('config', process.env.NEXT_PUBLIC_GA_ID, {
+      page_path: url,
+    });
+  }
+};
+
+export const event = ({ action, category, label, value }) => {
+  if (typeof window?.gtag !== 'undefined') {
+    window?.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+};
